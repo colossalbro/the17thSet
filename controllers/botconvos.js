1053f2ecf3a29c4586b8620359b7725dea87b595
colossal <emailagbawhe@gmail.com>
2022-12-26 20:51:08 +0100

    fixed annoying bug

--- controllers/botconvos.js
+++ controllers/botconvos.js
@@ -1,26 +1,37 @@
 const convoPicUrl = require("../utils/downloadUrl.js");
+const {tryAgain} = require("../utils/buttons.js");
 
-async function getMatricNo(conversation, ctx) {
-    await ctx.reply("Tell me your matriculaion number");
-    var res = await conversation.waitFor(":text");
+
+async function getMatricNo(conversation, ctx, next) {
+    
+    await ctx.reply("Tell me your matriculaion number/name. If you're going to tell me your name, please make sure its in this format: surname firstname");
+    
+    var res = await conversation.waitFor("msg");
+    
     var matno = res.message.text.toLowerCase()
 
     const matUrl = convoPicUrl(matno);
 
     if (!matUrl) {
-        ctx.reply("Im sorry, i can't seem to find your picture. If I still can't find your picture after you try again \
+        await ctx.reply("Im sorry, i can't seem to find your picture. If I still can't find your picture after you try again \
 consider messaging @danieljesusegun", tryAgain());
         return;
     }
     
-    await ctx.reply("Checking....");
+    await ctx.reply("checking ⏳");
 
-    await ctx.api.sendDocument(ctx.chat.id, matUrl, {
-        disable_content_type_detection : true,
-        caption : "👌🏾"
-    });
-    
-    return ctx.reply("Here you go! ❤️");
+    try{
+        await ctx.api.sendDocument(ctx.chat.id, matUrl, {
+            disable_content_type_detection : true,
+            caption : "Here you go! ❤"
+        });
+        
+    }catch(e) {
+        await ctx.reply(`${ctx.chat.first_name}, there seems to be an error downloading your potrait 🥺. Please message @danieljesusegun.`);
+        throw e;
+    }
+   
 }
+
 
 module.exports = getMatricNo;
