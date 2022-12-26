15fe59e34b5826e6df1f57e038588eca251858cf
colossal <emailagbawhe@gmail.com>
2022-12-26 20:52:19 +0100

    added if statemet for comp science

--- utils/downloadUrl.js
+++ utils/downloadUrl.js
@@ -1,11 +1,18 @@
 const data = require("../database/database.json")
 
 function convoPicUrl(matno) {
+    const comp = matno.slice(0, 4);
+    
+    if (comp === '17cg' || comp === '18cg') {
+        
+        const search = matno.slice(-3) + '.JPG';
+
+        if (data[search]) return data[search]
+    }
+    
     const jpg = matno.toUpperCase() + '.JPG';
     
-    if (data[jpg]) {
-        return data[jpg]
-    }
+    if (data[jpg]) return data[jpg]
 
     return null 
 }
